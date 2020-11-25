129bb4be270b01e189c22fe7cb8976860bf49938
Mitchell Mullich <mitchellmullich@gmail.com>
2020-11-25 10:39:14 -0800

    Last of the broken files were fixed

--- week2_1/sketch.js
+++ week2_1/sketch.js
@@ -1,7 +1,59 @@
-html, body {
-  margin: 0;
-  padding: 0;
+var centerX;      // declaring a global variable,
+var centerY;      // this is done outside of a
+var speedX;       // function
+var speedY;
+var centerX2;
+var centerY2;
+var speedX2;
+var speedY2;
+
+function setup() {
+  createCanvas(400, 400);
+  centerX = 200;  // assigning its initial value,
+  centerY = 200;  // in setup
+  speedX = 0;
+  speedY = 5;
+  centerX2 = 200;
+  centerY2 = 200;
+  speedX2 = -2;
+  speedY2 = -5;
 }
-canvas {
-  display: block;
+
+function draw() {
+  //background(255);
+
+  // update the speed (gravity pulls it down)
+  speedX = speedX + 0.1;
+  speedX2 = speedX2 - 0.1;
+  
+  // this conditional makes it change its
+  // direction (= negative speed) when it
+  // is hitting the floor
+  if (centerX > 350) {
+    speedX = speedX * -0.96;
+  } 
+  
+  if (centerX2 < 50) {
+  speedX2 = speedX2 * -0.96;
+  }
+  
+
+  // update the position
+  centerY = centerY + speedY;
+  centerX = centerX + speedX;
+  centerY2 = centerY2 + speedY2;
+  centerX2 = centerX2 + speedX2;
+
+  // this conditional makes it wrap around
+  // the canvas
+  if (centerY > 500) {
+    centerY = -100;
+  }
+  if (centerY2 < -100) {
+  centerY2 = 500;
+  }
+  //centerX2 = centerX2 - 1;
+  
+  rect(centerX, centerY, 50, 50);
+  rect(centerX2, centerY2, 50, 50);
 }
